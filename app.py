1918e2f228555856a5b547e00603dbe447082b4c
Yann Le Merrer <lemerrer.yann@protonmail.com>
2024-08-16 18:59:03 +0200

    affiche la liste des recettes importées

--- app.py
+++ app.py
@@ -2,8 +2,12 @@ from flask import Flask
 from flask import request
 from flask import render_template
 from werkzeug.utils import secure_filename
+import csv
+
+from recipe import Recipe
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
@@ -11,11 +15,20 @@ def home():
 
 
 @app.route("/recipes", methods=['POST'])
-def create_recipe():
+def load_recipes():
     file = request.files['recipes']
     filename = secure_filename(file.filename)
     path = f"/home/pascal/menus/uploads/{filename}"
     file.save(path)
-    with open(path, "r") as f:
-        print(f.readlines())
-    return "<p>Recettes reçues</p>"
+    recipes = []
+    with open(path, "r") as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        next(csv_reader, None)  # Ignore l'en-tête
+        for row in csv_reader:
+            recipe = create_recipe(row)
+            recipes.append(recipe)
+    return render_template('recipes.html', recipes=recipes)
+
+
+def create_recipe(row: list[str]) -> Recipe:
+    return Recipe(title=row[0], ingredients=row[6], steps=row[7], image=row[10])
---
+++ recipe.py
@@ -0,0 +1,6 @@
+class Recipe :
+    def __init__(self, *, title:str, ingredients:list[str], steps:list[str], image:str):
+        self.title = title
+        self.ingredients = ingredients
+        self.steps = steps
+        self.image = image
